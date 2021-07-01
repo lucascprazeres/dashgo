439dff46a40368d28c487272962d803a9c59632d
Lucas Prazeres <olucascostadp@gmail.com>
2021-07-01 13:10:21 -0300

    ref: make header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,22 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileDetails: boolean;
+}
+
+export function Profile({ showProfileDetails = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>
-          Lucas Prazeres
+      { showProfileDetails && (
+        <Box mr="4" textAlign="right">
+          <Text>
+            Lucas Prazeres
             </Text>
-        <Text color="gray.300" fontSize="small">
-          lucas.prazeres@dashgo.com
-            </Text>
-      </Box>
+          <Text color="gray.300" fontSize="small">
+            lucas.prazeres@dashgo.com
+        </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Lucas Prazeres" src="https://github.com/lucascprazeres.png" />
     </Flex>
